ca56533ff4836506b47ab27b572ef7cd63398e8c
Kiran More <kiraan.more@gmail.com>
2023-01-21 19:12:39 +0000

    Service Catalog product for VPC

---
+++ product-developer/main.tf
@@ -0,0 +1,37 @@
+resource "random_string" "random" {
+  length           = 16
+  special          = false
+  lower            = true
+  upper            = false
+}
+
+resource "aws_s3_bucket" "product_template_bucket" {
+  bucket = "${var.bucket_name}-${random_string.random.id}"
+}
+
+resource "aws_s3_bucket_object" "product_template" {
+  bucket = aws_s3_bucket.product_template_bucket.id
+  key    = "template.yaml"
+  source = "./template.yaml"
+}
+
+resource "aws_servicecatalog_portfolio" "portfolio" {
+  name          = var.portfolio_name
+  description   = var.portfolio_description
+  provider_name = var.provider_name
+}
+
+resource "aws_servicecatalog_product" "product" {
+  name  = var.product_name
+  owner = var.product_owner
+  type  = "CLOUD_FORMATION_TEMPLATE"
+
+  provisioning_artifact_parameters {
+    template_url = "https://s3.amazonaws.com/${aws_s3_bucket.product_template_bucket.id}/${aws_s3_bucket_object.product_template.id}"
+  }
+}
+
+resource "aws_servicecatalog_product_portfolio_association" "product_portfolio_association" {
+  portfolio_id = aws_servicecatalog_portfolio.portfolio.id
+  product_id   = aws_servicecatalog_product.product.id
+}
---
+++ product-developer/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {    
+    type = string
+}
+
+variable "bucket_name" {    
+    type = string
+}
+
+variable "portfolio_name" {    
+    type = string
+}
+
+variable "provider_name" {    
+    type = string
+}
+
+variable "portfolio_description" {    
+    type = string
+}
+
+variable "product_name" {    
+    type = string
+}
+
+variable "product_owner" {    
+    type = string
+}
